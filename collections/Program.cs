b9db7a5507470724f78d57c20ec43660f6dc4a3e
Sun Davey <SunDavey@hotmail.com>
2018-04-01 21:38:38 -0700

    added a few more examples

--- collections/Program.cs
+++ collections/Program.cs
@@ -9,7 +9,16 @@ namespace collections
         {
             Console.WriteLine("Collection Practice");
             int[] basicArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
             string[] nameArr = { "Tim", "Martin", "Nikki", "Sara" };
+            string[] IcecreamFlavors = { "Mint Chocolate", "Fudge double Chocolate", "Strawberry Banana", "Earl Grey", "Coffee", "Caramel Sea salt", "Green Tea", "Red bean", "Vanila Beans" };
+            customerFlavors(nameArr, IcecreamFlavors);
+
+            multiD();
+        }
+
+        static void trueFalse()
+        {
             bool[] boolArr = new bool[10];
             for (int i = 0; i < 10; i++)
             {
@@ -26,31 +35,58 @@ namespace collections
             {
                 System.Console.WriteLine(i);
             }
-            IcecreamFlavors();
-
         }
 
-        // static Array multiD(int rows, int cols, int items){
-        //     int [,,] array3d = new int [10, 1, 10];
-        //     for(int i = 0; i < rows; i++){
-        //         for (int j = 0; j < items; j++){
-        //             array3d[i][j] = 
-        //         }
-        //     }
-        // }
-
-        static void IcecreamFlavors()
+        static void multiD()
         {
-            string[] IcecreamFlavors = { "Mint Chocolate", "Pudge double Chocolate", "Strawberry Banana", "Earl Grey", "Coffee", "Caramel Sea salt", "Green Tea", "Red bean", "Vanila Beans" };
-            List<string> icecream = new List<string>(IcecreamFlavors.Length);
-            for (var i = 0; i < IcecreamFlavors.Length; i++)
+            int[,] array2d = new int[10, 10];
+            int count = 0;
+
+            for (int j = 0; j < 10; j++)
             {
-                icecream.Add(IcecreamFlavors[i]);
+                Console.Write("[ ");
+                count++;
+                for (int i = 0; i < 10; i++)
+                {
+                    array2d[i, j] = count * (i + 1);
+                    Console.Write(count * (i + 1) + ", ");
+                }
+                Console.Write("] ");
+                System.Console.WriteLine();
+            }
+        }
+        static void IcecreamFlavors(string[] arr)
+        {
+            List<string> icecream = new List<string>(arr.Length);
+            for (var i = 0; i < arr.Length; i++)
+            {
+                icecream.Add(arr[i]);
             }
             System.Console.WriteLine("The length of Icecream Flavors array: " + icecream.Count);
             System.Console.WriteLine("The third flavor: " + icecream[2]);
             icecream.Remove(icecream[2]);
             System.Console.WriteLine("The length of Icecream Flavors array: " + icecream.Count);
+        }
+
+        static void customerFlavors(string[] customers, string[] flavors)
+        {
+            Random rand = new Random();
+            Dictionary<string, string> customersFlavors = new Dictionary<string, string>(customers.Length);
+            for (int i = 0; i < customers.Length; i++)
+            {
+                customersFlavors.Add(customers[i], null);
+            }
+            
+            for (int i = 0; i < customers.Length; i++)
+            {
+                int flavorIndex = rand.Next(0, flavors.Length);
+                customersFlavors[customers[i]] = flavors[flavorIndex];
+            }
+            
+            foreach(var customer in customersFlavors){
+                System.Console.WriteLine(customer.Key + " - " + customer.Value);
+            }
+            
         }
     }
 }
